78b1640cf7cab955040f0675f159aebd91ccb922
Karin Alvarez <karin@Josiah-Haswells-MacBook.local>
2014-03-30 01:35:28 -0600

    Link icon functionality

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ if ('development' == app.get('env')) {
 app.get('/', home.index);
 
 
+
+
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
